1813e78ec758c49d859ba7caed390dffeacb0466
Ines Franco <ines_fra@hotmail.com>
2021-11-23 15:07:09 +0000

    touched up on the create script

--- scripts/create.sql
+++ scripts/create.sql
@@ -1,36 +1,32 @@
 CREATE TABLE activo
 (
-    id INT NOT NULL,
+    activo_id INT NOT NULL PRIMARY KEY,
     nome VARCHAR NOT NULL,
     data_aquisicao DATE NOT NULL,
     state Bit NOT NULL,
     marca VARCHAR,
     modelo VARCHAR,
-    tipo INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (tipo) REFERENCES tipo_activo
+    tipo INT FOREIGN KEY REFERENCES tipo_activo,
 )
 
 CREATE TABLE activo_gerente(
-    id_activo INT,
-    id_gerente INT,
-    FOREIGN KEY (id_activo) REFERENCES activo,
-    FOREIGN KEY (id_gerente) REFERENCES funcionario
+    id_activo INT FOREIGN KEY REFERENCES activo,
+    id_gerente INT FOREIGN KEY REFERENCES funcionario,
+    PRIMARY KEY (id_activo, id_gerente)
 )
 
 
 CREATE TABLE tipo_activo(
-    id_tipo INT,
-    description VARCHAR(50),
-    PRIMARY KEY (id_tipo)
+    id_tipo INT PRIMARY KEY,
+    description VARCHAR(50)
 )
 
-CREATE TABLE activo_preco
+CREATE TABLE activo_preçocomercial
 (
-    id_activo INT NOT NULL,
+    id_activo INT NOT NULL FOREIGN KEY REFERENCES activo,
     preco FLOAT NOT NULL,
     data_alteracao DATE NOT NULL,
-    FOREIGN KEY (id_activo) REFERENCES activo
+    PRIMARY KEY (id_activo, data_alteracao)
 )
 
 CREATE TABLE activo_filho
@@ -38,11 +34,13 @@ CREATE TABLE activo_filho
     id_activo_pai INT NOT NULL,
     id_activo_filho INT NOT NULL,
     FOREIGN KEY (id_activo_pai) REFERENCES activo,
-    FOREIGN KEY (id_activo_filho) REFERENCES activo
+    FOREIGN KEY (id_activo_filho) REFERENCES activo,
+    PRIMARY KEY (id_activo_pai, id_activo_filho)
 )
 
 CREATE TABLE funcionario
 (
+    id_funcionario INT NOT NULL PRIMARY KEY ,
     numero_identificacao INT NOT NULL,
     nome VARCHAR(50),
     data_nascimento DATE,
@@ -50,20 +48,55 @@ CREATE TABLE funcionario
     profissao VARCHAR(15),
     telefone INT,
     email VARCHAR check(email LIKE '%_@__%.__%'),
-    id_competencia INT,
-    FOREIGN KEY (id_competencia) REFERENCES elemento_equipa_competencia
-    PRIMARY KEY (numero_identificacao)
 )
 
-
-//todo
-CREATE TABLE elemento_equipa_competencia
+CREATE TABLE competencia
 (
-    funcionario_id INT NOT NULL,
-    id_competencia INT NOT NULL,
-    FOREIGN KEY (id_competencia) REFERENCES elemento_equipa_competencia
-    PRIMARY KEY (numero_identificacao)
+    id_competencia INT NOT NULL PRIMARY KEY ,
+    descricao VARCHAR NOT NULL
 )
 
 
+CREATE TABLE funcionario_competencia
+(
+    id_funcionario INT NOT NULL,
+    id_competencia INT NOT NULL,
+    FOREIGN KEY (id_competencia) REFERENCES competencia,
+    FOREIGN KEY (id_funcionario) REFERENCES funcionario,
+    PRIMARY KEY(id_funcionario,id_competencia)
+)
 
+
+CREATE TABLE intervencao
+(
+    id_intervencao INT NOT NULL PRIMARY KEY,
+    descricao VARCHAR NOT NULL,
+    estado VARCHAR NOT NULL,
+    valor FLOAT NOT NULL,
+    data_inicio DATE NOT NULL,
+    data_fim DATE NOT NULL,
+)
+
+
+CREATE TABLE equipa
+(
+    codigo_equipa INT NOT NULL PRIMARY KEY,
+    localizacao VARCHAR NOT NULL,
+    num_elems INT NOT NULL check(num_elems >= 2),
+    id_supervisor INT NOT NULL
+)
+
+CREATE TABLE funcionario_equipa
+(
+    codigo_equipa INT NOT NULL FOREIGN KEY REFERENCES equipa,
+    id_funcionario INT NOT NULL FOREIGN KEY REFERENCES funcionario,
+    PRIMARY KEY(codigo_equipa, id_funcionario)
+)
+
+CREATE TABLE intervencao_equipa
+(
+    codigo_equipa INT NOT NULL FOREIGN KEY REFERENCES equipa,
+    id_intervencao INT NOT NULL FOREIGN KEY REFERENCES intervencao,
+    data_inicio DATE NOT NULL,
+    PRIMARY KEY (codigo_equipa, id_intervencao)
+)
