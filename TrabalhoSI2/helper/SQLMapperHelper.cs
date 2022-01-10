37120070ac8214164fe38b2b04be25740e279c10
InesFranco <ines_fra@hotmail.com>
2022-01-10 15:49:20 +0000

    started

---
+++ TrabalhoSI2/helper/SQLMapperHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoSI2.helper
+{
+    public class SQLMapperHelper
+    {
+
+        //Delegate to MAP an entity from a row
+        public delegate T Mapper<T>(IDataRecord data);
+        public static int ExecuteNonQuery(string connectionString, string cmdtxt, IDbDataParameter[] dbDataParameters)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = con.CreateCommand())
+                {
+                    cmd.CommandText = cmdtxt;
+                    cmd.Parameters.AddRange(dbDataParameters);
+                    con.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static T ExecuteScalar<T>(string connectionString, string cmdtxt, IDbDataParameter[] dbDataParameters)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = con.CreateCommand())
+                {
+                    cmd.CommandText = cmdtxt;
+                    cmd.Parameters.AddRange(dbDataParameters);
+                    con.Open();
+                    return (T)cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        public static T ExecuteMapSingle<T>(string connectionString, string cmdtxt, IDbDataParameter[] dbDataParameters, Mapper<T> map)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = con.CreateCommand())
+                {
+                    cmd.CommandText = cmdtxt;
+                    cmd.Parameters.AddRange(dbDataParameters);
+                    con.Open();
+                    using (SqlDataReader rd = cmd.ExecuteReader())
+                    {
+                        if (rd.Read())
+                        {
+                            return map((IDataRecord)rd);
+
+                        }
+                    }
+                }
+            }
+            return default;
+        }
+
+        public static TCol ExecuteMapSet<T, TCol>(string connectionString, string cmdtxt, IDbDataParameter[] dbDataParameters, Mapper<T> map)
+            where TCol : class, IList, new()
+        {
+            //TODO
+            throw new System.NotImplementedException();
+        }
+
+        }
+    }
+}
---
+++ TrabalhoSI2/mapper/IMapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoSI2.mapper
+{
+    public interface IMapper<T,Tid>
+    {
+        T Create(T entity);
+        T Read(Tid id);
+        T Update(T entity);
+        T Delete(T entity);
+    }
+}
